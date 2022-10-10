1b6ad253bbc23ffdc44ec58144e4238cd9681e7f
Imrnkhan <imrankhan7581@gmail.com>
2022-10-10 16:01:47 +0530

    d

---
+++ Emp_ApI/index.js
@@ -0,0 +1,46 @@
+// api url
+const api_url ="https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees";
+
+// Defining async function
+async function getapi(url) {
+	
+	// Storing response
+	const response = await fetch(url);
+	
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		hideloader();
+	}
+	show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to hide the loader
+function hideloader() {
+	document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+	let tab =
+		`<tr>
+		<th>Name</th>
+		<th>Office</th>
+		<th>Position</th>
+		<th>Salary</th>
+		</tr>`;
+	
+	// Loop to access all rows
+// 	for (let r of data.list) {
+// 		tab += `<tr>
+// 	<td>${r.name} </td>
+// 	<td>${r.office}</td>
+// 	<td>${r.position}</td>
+// 	<td>${r.salary}</td>		
+// </tr>`;
+// 	}
+	// Setting innerHTML as tab variable
+	document.getElementById("employees").innerHTML = tab;
+}
